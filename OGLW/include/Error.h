20c9ea280d6b558d683befd8302b48e27c1e59f9
NetCon <aiprog@free.fr>
2010-10-12 08:57:21 +0200

    Remise en place du Readme+CDC

--- OGLW/include/Error.h
+++
@@ -1,8 +0,0 @@
-#ifndef ERROR_H
-#define ERROR_H
-
-#include <stdio.h>
-
-void fatalError(char *message);
-
-#endif // ERROR_H
--- OGLW/src/Error.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/Error.h"
-
-void fatalError(char *message)
-{
-  fprintf(stderr, "main: %s\n", message);
-}
